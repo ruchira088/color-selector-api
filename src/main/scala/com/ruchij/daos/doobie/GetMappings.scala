862967ab5145d7341202a8968ff11717ad8b151d
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-04-06 01:06:03 +1000

    Updating the Doobie DateTime mapper

--- src/main/scala/com/ruchij/daos/doobie/GetMappings.scala
+++ src/main/scala/com/ruchij/daos/doobie/GetMappings.scala
@@ -1,10 +1,12 @@
 package com.ruchij.daos.doobie
 
+import doobie.implicits.javasql.TimestampMeta
 import doobie.util.Get
 import enumeratum.{Enum, EnumEntry}
 import org.joda.time.DateTime
 
-import java.util.{Date, UUID}
+import java.sql.Timestamp
+import java.util.UUID
 import scala.util.Try
 
 object GetMappings {
@@ -14,7 +16,7 @@ object GetMappings {
       value => enumValue.withNameInsensitiveEither(value).left.map(_.getMessage())
     }
 
-  implicit val dateTimeGet: Get[DateTime] = Get[Date].map { date => new DateTime(date.getTime) }
+  implicit val dateTimeGet: Get[DateTime] = Get[Timestamp].map { date => new DateTime(date.getTime) }
 
   implicit val uuidGet: Get[UUID] =
     Get[String].temap { value => Try(UUID.fromString(value)).toEither.left.map(_.getMessage) }
--- src/main/scala/com/ruchij/daos/doobie/PutMappings.scala
+++ src/main/scala/com/ruchij/daos/doobie/PutMappings.scala
@@ -1,17 +1,19 @@
 package com.ruchij.daos.doobie
 
 import doobie.util.Put
+import doobie.implicits.javasql.TimestampMeta
 import enumeratum.EnumEntry
 import org.joda.time.DateTime
 import org.tpolecat.typename.TypeName
 
+import java.sql.Timestamp
 import java.util.{Date, UUID}
 
 object PutMappings {
 
   implicit def enumPut[A <: EnumEntry: TypeName]: Put[A] = Put[String].tcontramap[A](_.entryName)
 
-  implicit val dateTimePut: Put[DateTime] = Put[Date].tcontramap(dateTime => new Date(dateTime.getMillis))
+  implicit val dateTimePut: Put[DateTime] = Put[Timestamp].tcontramap(dateTime => new Timestamp(dateTime.getMillis))
 
   implicit val uuidPut: Put[UUID] = Put[String].tcontramap[UUID](_.toString)
 
